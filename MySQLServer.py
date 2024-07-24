8e8071f01a44b46b701e288ead68b0a5324b894b
Walter Isaac <walter4real40@gmail.com>
2024-07-24 09:13:11 +0100

    commiting MySQLServer.py

---
+++ MySQLServer.py
@@ -0,0 +1,14 @@
+from mysql.connector import connect, Error
+
+try:
+    with connect(
+        host="localhost",
+        user="root",
+        password="theartoflyingXXX1$"
+    ) as connection:
+        create_db_query = "CREATE DATABASE IF NOT EXISTS alx_book_store"
+        with connection.cursor() as cursor:
+            cursor.execute(create_db_query)
+            print ("Database 'alx_book_store' created succesfully!")
+except Error as e:
+    print(e)
