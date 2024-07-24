538c11969f17e766dbdfdd89e281949be856c213
Walter Isaac <walter4real40@gmail.com>
2024-07-24 10:37:33 +0100

    commiting task_5.sql

---
+++ task_5.sql
@@ -0,0 +1,8 @@
+# SQL script to insert a single row into the customer table
+
+# Specify the database
+USE alx_book_store;
+
+# Insert statement
+INSERT INTO customer (customer_id, customer_name, email, address)
+VALUES ('1', 'Cole Baidoo', 'johndoe@example.com', '123 Happiness Ave.');
