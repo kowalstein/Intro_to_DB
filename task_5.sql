227bcfad97f3b417a6b4c9e43bca5430e13b9cda
Walter Isaac <walter4real40@gmail.com>
2024-07-24 10:39:36 +0100

    corrected email value

--- task_5.sql
+++ task_5.sql
@@ -5,4 +5,4 @@ USE alx_book_store;
 
 # Insert statement
 INSERT INTO customer (customer_id, customer_name, email, address)
-VALUES ('1', 'Cole Baidoo', 'johndoe@example.com', '123 Happiness Ave.');
+VALUES ('1', 'Cole Baidoo', 'cbaidoo@sandtech.com', '123 Happiness Ave.');
